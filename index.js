9dbfa0b77c2151780f34b8ed0cb3663d4a21c375
kamron-hasanzoda <116718755+kamron-hasanzoda@users.noreply.github.com>
2022-11-02 13:46:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Math
+// GitHUB
+
+let a = "20.9999"
+let b = "21"
+
+
+console.log(
+    // Math.floor(a) // окргуляет вниз
+    // Math.ceil(a) // окргуляет вверх
+    // Math.round(a) // окргуляет логически
+
+    // Math.max(a, b, 12, 55, -123)
+    // Math.min(a, b, 12, 55, -123)
+    // Math.floor(Math.random() * 13)
+    // Math.pow(7, 2)
+
+    // parseInt("20.9999")
+    // parseFloat(a)
+);
